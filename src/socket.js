d864dfe42eba1f4766f85d774b1991fb3afca7ca
GytRag <gytis.raginskas@gmail.com>
2025-04-07 12:01:36 +0300

    up api

--- src/socket.js
+++ src/socket.js
@@ -3,3 +3,4 @@ import  {io} from 'socket.io-client'
 
 export const socket = io('http://213.136.82.182:3011');
 // export const socket = io('http://localhost:3011');
+// export const socket = io('/socket');
