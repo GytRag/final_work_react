d2df38b6633a625cc1d76a884e55e1438f10267b
GytRag <gytis.raginskas@gmail.com>
2025-04-07 15:08:35 +0300

    socket up

--- src/socket.js
+++ src/socket.js
@@ -4,6 +4,7 @@ import  {io} from 'socket.io-client'
 // export const socket = io('http://213.136.82.182:3011');
 // export const socket = io('http://localhost:3011');
 // export const socket = io('/socket');
-export const socket = io('http://213.136.82.182:3011', {
+export const socket = io('/', {
+    path: '/socket.io',
     transports: ['websocket'],
 });
